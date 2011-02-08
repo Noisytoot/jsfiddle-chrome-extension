9d5be61300af0bf97937d91ec3fa4e4d8ce80759
Oliver Caldwell <oliver@flowdev.co.uk>
2011-02-08 21:09:07 +0000

    Made it try to grab the users jsFiddle name automatically, if that fails they heve to do it the normal way. I have also moved all of the fiddle setup code into its own function so it can be run after auto configuration.

--- main.js
+++ main.js
@@ -1,59 +1,24 @@
 Spark.ready(function() {
 	// Check if they need to configure the extension
 	if(localStorage['username'] === undefined) {
-		// Show the 'You need to configure' message
-		Spark('div#configure').transition('fadein');
+		// Try to grab it automatically
+		Spark.ajax('get', 'http://jsfiddle.net/user/get_username/', false, function(username) {
+			if(username == '') {
+				// Show the 'You need to configure' message
+				Spark('div#configure').transition('fadein');
+			}
+			else {
+				// Auto configure the username
+				localStorage['username'] = username;
+				
+				// Load their fiddles
+				setupFiddles();
+			}
+		});
 	}
 	// Otherwise, load their fiddles
 	else {
-		// Make the asynchronous AJAX call
-		Spark.ajax('GET', 'http://jsfiddle.net/api/user/' + localStorage['username'] + '/demo/list.json', 'limit=100', function(json) {
-			// Set up the variables
-			var built = '';
-			var revision = '';
-			
-			// Decode the JSON retrieved by the API
-			var list = Spark.json('decode', json);
-			
-			// Loop through all of the fiddles in the list
-			for(var l in list) {
-				// If there is a history show the latest one (this needs tweeking)
-				if(list[l].latest_version > list[l].version) {
-					revision = ' (<a href="' + list[l].url + list[l].latest_version + '/" target="_blank">Revision ' + list[l].latest_version + '</a>)'
-				}
-				else {
-					revision = '';
-				}
-				
-				// Build the HTML string (I know its a mess, I will clean it up soon)
-				built += '<li><a href="' + list[l].url + '" target="_blank"><strong>' + list[l].title + revision + '</strong></a>' + ((list[l].description != '') ? ' - ' + list[l].description : '') + '<br /><a href="javascript:void(0)" class="toggle" id="e' + l + '">Preview</a><iframe id="e' + l + '" style="display: none">' + list[l].url + list[l].latest_version + '/show/</iframe></li>';
-			}
-			
-			// Drop the built HTML into the UL
-			Spark('ul').html(built);
-			
-			// Fade the fiddles in
-			Spark('div#viewFiddle').transition('fadein', 300, function() {
-				// Listen for a click of a toggle editor link
-				Spark('a.toggle').event('click', function(e) {
-					// Get the iframe
-					var el = Spark('iframe#' + e.target.id).attribute();
-					
-					// Check if we need to show or hide
-					if(Spark(el).css().display == 'none') {
-						// Load the page
-						el.src = el.innerHTML;
-						
-						// Show the appropriate iFrame
-						Spark(el).transition('fadein');
-					}
-					else {
-						// Hide the appropriate iFrame
-						Spark(el).transition('fadeout');
-					}
-				});
-			});
-		});
+		setupFiddles();
 	}
 	
 	// Set up the transitions between editing and viewing fiddles
@@ -69,6 +34,58 @@ Spark.ready(function() {
 		});
 	});
 });
+
+// Grabs all of the fiddles, builds the html and displays it.
+function setupFiddles() {
+	// Make the asynchronous AJAX call
+	Spark.ajax('GET', 'http://jsfiddle.net/api/user/' + localStorage['username'] + '/demo/list.json', 'limit=100', function(json) {
+		// Set up the variables
+		var built = '';
+		var revision = '';
+		
+		// Decode the JSON retrieved by the API
+		var list = Spark.json('decode', json);
+		
+		// Loop through all of the fiddles in the list
+		for(var l in list) {
+			// If there is a history show the latest one (this needs tweeking)
+			if(list[l].latest_version > list[l].version) {
+				revision = ' (<a href="' + list[l].url + list[l].latest_version + '/" target="_blank">Revision ' + list[l].latest_version + '</a>)'
+			}
+			else {
+				revision = '';
+			}
+			
+			// Build the HTML string (I know its a mess, I will clean it up soon)
+			built += '<li><a href="' + list[l].url + '" target="_blank"><strong>' + list[l].title + revision + '</strong></a>' + ((list[l].description != '') ? ' - ' + list[l].description : '') + '<br /><a href="javascript:void(0)" class="toggle" id="e' + l + '">Preview</a><iframe id="e' + l + '" style="display: none">' + list[l].url + list[l].latest_version + '/show/</iframe></li>';
+		}
+		
+		// Drop the built HTML into the UL
+		Spark('ul').html(built);
+		
+		// Fade the fiddles in
+		Spark('div#viewFiddle').transition('fadein', 300, function() {
+			// Listen for a click of a toggle editor link
+			Spark('a.toggle').event('click', function(e) {
+				// Get the iframe
+				var el = Spark('iframe#' + e.target.id).attribute();
+				
+				// Check if we need to show or hide
+				if(Spark(el).css().display == 'none') {
+					// Load the page
+					el.src = el.innerHTML;
+					
+					// Show the appropriate iFrame
+					Spark(el).transition('fadein');
+				}
+				else {
+					// Hide the appropriate iFrame
+					Spark(el).transition('fadeout');
+				}
+			});
+		});
+	});
+}
 
 // This function is run when the framework or dependencies are changed, it appends the forms action
 function changeAction(framework, dependencies) {
